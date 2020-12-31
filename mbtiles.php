7b78b824cf1b756a633ce40f67e714e6a13ea89d
Adrian B. Hoffsten <asd20752@users.noreply.github.com>
2020-12-31 22:14:46 +0100

    Backwards traversing folders. File exsists
    Added checking for backwards traversing of folders.
    Also added checking if the database exsists. This to prevent the server from creating new databases when no file exsists.

--- mbtiles.php
+++ mbtiles.php
@@ -1,8 +1,11 @@
 <?php
-$zoom = $_GET['z'];
-$column = $_GET['x'];
-$row = $_GET['y'];
+$zoom = (int)$_GET['z'];
+$column = (int)$_GET['x'];
+$row = (int)$_GET['y'];
 $db = $_GET['db'];
+
+//Check if the file requested exsists and is in the working directory or it's subfolders
+if (substr(getcwd(), 0, strlen(realpath($db))) === getcwd() && file_exists($db)) {
   try
   {
     // Open the database
@@ -11,6 +14,7 @@ $db = $_GET['db'];
 	// Query the tiles view and echo out the returned image
 	$sql = "SELECT * FROM tiles WHERE zoom_level = :zoom AND tile_column = :column AND tile_row = :row";
 	$q = $conn->prepare($sql);
+	//Bind the parameters to prevent SQL injection
 	$q->bindParam(":zoom", $zoom);
 	$q->bindParam(":column", $column);
 	$q->bindParam(":row", $row);
@@ -32,4 +36,6 @@ $db = $_GET['db'];
   {
     print 'Exception : '.$e->getMessage();
   }
-?>
+} else {
+	http_response_code(404);
+}
